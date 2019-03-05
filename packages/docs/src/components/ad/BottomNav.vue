73beb3af70d3f385be67a6d09b073cb9c7cf86ef
Michał Parkitny <kontakt@michalparkitny.com>
2019-03-05 15:51:28 +0100

    Prepared repo

--- packages/docs/src/components/ad/BottomNav.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <v-bottom-nav
-    :value="true"
-    app
-    height="48"
-  >
-    <slot />
-  </v-bottom-nav>
-</template>
--- packages/docs/src/snippets/js/transpileDependencies.txt
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  transpileDependencies:[/node_modules[/\\\\]vuetify[/\\\\]/]
-}
